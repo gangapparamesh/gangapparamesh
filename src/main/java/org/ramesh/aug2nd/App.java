8363a1f09fd513d1cc72326ec03dbd852c53cd1a
Dell <Dell@Dell-PC>
2017-10-27 15:16:32 +0530

    first commit

---
+++ src/main/java/org/ramesh/aug2nd/App.java
@@ -0,0 +1,13 @@
+package org.ramesh.aug2nd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/ramesh/aug2nd/NewTest.java
@@ -0,0 +1,48 @@
+package org.ramesh.aug2nd;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+/*import com.relevantcodes.extentreports.LogStatus;
+import com.wd.utils.ReportUtil;*/
+
+public class NewTest {
+	
+WebDriver driver;
+	@Test
+	public void login() {
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://opensource.demo.orangehrmlive.com");
+		driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys("admin");
+		driver.findElement(By.xpath("//input[@id=\"txtPassword\"]")).sendKeys("admin");
+		driver.findElement(By.xpath("//input[@id=\"btnLogin\"]")).click();
+
+	}
+
+	@Test(dependsOnMethods = "login")
+	public void addEmployee() throws InterruptedException {
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//a[contains(.,'PIM')]")).click();
+		driver.findElement(By.xpath("//a[contains(text(),'Add Emp')]")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//input[@id=\"firstName\"]")).sendKeys("sel12");
+		driver.findElement(By.xpath("//*[@id=\"lastName\"]")).sendKeys("sel1");
+		driver.findElement(By.xpath("//input[@id='btnSave']")).click();
+		driver.close();
+	}
+
+	@AfterClass
+	public void closeApplication() {
+		//ohrm.closeApplication();
+		driver.close();
+	}
+}
